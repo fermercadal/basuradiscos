f3d11681974d566e200eea04ea0a88dbde7f7936
Fer Mercadal <fermercadal.1@gmail.com>
2024-07-20 21:47:34 -0300

    chore: :wrench: Add prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,24 @@
 import { defineCollection, z } from "astro:content";
 
 const catalog = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		artist: z.string(),
-		category: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		artFront: z.string(),
-		artBack: z.string(),
-		aboutMusic: z.string().optional(),
-		aboutStudio: z.string().optional(),
-		aboutArtwork: z.string().optional(),
-		linkMp3: z.string().optional(),
-		linkFlac: z.string().optional(),
-		linkYoutube: z.string().optional(),
-		linkBandcamp: z.string().optional(),
-		linkSoundcloud: z.string().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    artist: z.string(),
+    category: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    artFront: z.string(),
+    artBack: z.string(),
+    aboutMusic: z.string().optional(),
+    aboutStudio: z.string().optional(),
+    aboutArtwork: z.string().optional(),
+    linkMp3: z.string().optional(),
+    linkFlac: z.string().optional(),
+    linkYoutube: z.string().optional(),
+    linkBandcamp: z.string().optional(),
+    linkSoundcloud: z.string().optional(),
+  }),
 });
 
 export const collections = { catalog };
